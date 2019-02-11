1e24ec7e2ed87f0916fa4588bd530f5f5f35f952
longsl <583297550@qq.com>
2019-02-12 00:36:34 +0800

    [+] spring-hello demo
    第一次提交，创建spring-hello demo项目

---
+++ src/test/java/com/longshilin/spring/springhello/SpringHelloApplicationTests.java
@@ -0,0 +1,17 @@
+package com.longshilin.spring.springhello;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringHelloApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
